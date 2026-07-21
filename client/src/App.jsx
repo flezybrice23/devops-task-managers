026dd716e30326c02b0b93d1854faf58fbc82cc2
Flezybrice <briceedobe@gmail.com>
2026-07-21 11:02:04 +0100

    Create React frontend and Express backend

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+
+function App(){
+
+const [message,setMessage] = useState("");
+
+
+useEffect(()=>{
+
+fetch("http://localhost:5000")
+.then(res=>res.json())
+.then(data=>{
+    setMessage(data.message);
+});
+
+},[]);
+
+
+return (
+<div>
+
+<h1>
+DevOps Task Manager
+</h1>
+
+
+<h2>
+{message}
+</h2>
+
+
+</div>
+)
+
+}
+
+
+export default App;
